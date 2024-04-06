86456916be397f6608a7ce1ac5eeb6b1c3e248d0
MrSongP <100014126+MrSongP@users.noreply.github.com>
2024-04-06 20:26:53 +0800

    Delete fsdfd directory

--- fsdfd/configs.py
+++
@@ -1,7 +0,0 @@
-save_dir                    = './'
-data_dir = {}
-data_dir['FACE']             = './filelists/FACE/'
-data_dir['CUB']             = './filelists/CUB/'
-data_dir['Dogs']             = './filelists/Dogs/'
-
-
